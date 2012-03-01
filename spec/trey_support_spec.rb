1e8f593c8dd65cae395e0de277fa92786b0f1679
Kyle Brett <kyle@vitrue.com>
2012-03-01 09:27:40 -0500

    Specced some requests, including array params.

--- spec/trey_support_spec.rb
+++ spec/trey_support_spec.rb
@@ -110,6 +110,24 @@ describe 'trey support' do
   def app; TreyAPI; end
 
   describe '/page_insights' do
+    req_params = {
+      page_ids: [1, 2, 3].join(' '),
+      metrics: ['these', 'those'].join(' '),
+      since: 'yesterday',
+      until: 'today'
+    }
+
+    opt_params = {
+      interval: 'week'
+    }
+
+    bad_params = {
+      post_ids: [4, 5, 6].join(' '),
+      limit: 100,
+      sort: 'these',
+      order: 'descending'
+    }
+
     it "responds 404 to a request with no parameters" do
       get '/page_insights' do |response|
         response.status.should == 404
@@ -119,16 +137,40 @@ describe 'trey support' do
       end
     end
 
-    it "responds 200 to a request with required parameters provided" do
-      # params = {
-      #   page_ids: [1, 2, 3],
-      #   metrics: ['these', 'those'],
-      #   since: 'yesterday',
-      #   until: 'today'
-      # }
+    it "responds 404 with only optional parameters" do
+      get('/page_insights', opt_params) do |response|
+        response.status.should == 404
+        errors = JSON.parse(response.body)['errors']['/page_insights']
+        errors.size.should == 4 # required params
+        errors.values.each {|v| v.should == 'missing_required_param'}
+      end
+    end
 
-      get('/page_insights?page_ids=1&metrics=these&since=yesterday&until=today') do |response|
+    it "responds 404 to a request with required parameters improperly formatted" do
+      get('/page_insights', req_params.merge(page_ids: 'string_not_array')) do |response|
+        response.status.should == 404
+        errors = JSON.parse(response.body)['errors']['/page_insights']
+        errors.size.should == 1
+        errors.should == {'page_ids' => 'type_mismatch'}
+      end
+    end
+
+    it "responds 200 to a request with required parameters provided" do
+      get('/page_insights', req_params) do |response|
         response.status.should == 200
+      end
+    end
+
+    it "allows optional parameters in addition to the required params" do
+      get('/page_insights', req_params.merge(opt_params)) do |response|
+        response.status.should == 200
+      end
+    end
+
+    it "understands array parameters" do
+      get('/page_insights', req_params) do |response|
+        response.status.should == 200
+        JSON.parse(response.body)['page_ids'].should == [1, 2, 3]
       end
     end
   end
