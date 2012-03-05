faaacf3a1102725d30b26bee0e1e7028e351375d
Kyle Brett <kyle@vitrue.com>
2012-03-05 15:54:51 -0500

    Cleanup Diesel::RackInterface a little

--- lib/diesel/rack_interface.rb
+++ lib/diesel/rack_interface.rb
@@ -8,58 +8,75 @@ module Diesel::RackInterface
 
   def call(env)
     out = nil
+    request = nil
     start_time = Time.now
-    RubyProf.start if PROFILE_REQUESTS
+    start_profile if PROFILE_REQUESTS
     begin
       request = Diesel::Request.new(env)
-      Diesel.logger.info "#{request.request_method} #{request.path}"
-      Diesel.logger.info request.params.inspect
       out = __router.handle_request(request)
     rescue Diesel::RespondWithHTTPBasicChallenge
-      response = Rack::Response.new
-      response['WWW-Authenticate'] = %(Basic realm="Diesel HTTP Basic")
-      response.status = 401
-      response.headers['Content-Type'] = 'text/html'
-      response.write("Not Authorized\n")
-      out = response.finish
+      out = respond_with_http_basic_challenge
     rescue StandardError => e
-      response = Rack::Response.new
-      response.headers['Content-Type'] = 'application/json'
-
-      begin
-        status, errors = JSON.parse(e.to_s) # TODO subclass StandardError
-        response.status = status;
-      rescue JSON::ParserError
-        response.status = 500
-        response.write({
-          status: 500,
-          error_class: e.class.to_s,
-          errors: e.to_s,
-          trace: e.backtrace
-        }.to_json)
-      else
-        response.write({
-          status: status,
-          error_class: 'Diesel Runtime Error',
-          errors: errors,
-          trace: e.backtrace
-        }.to_json)
-      end
-
-      out = response.finish
+      out = respond_with_error_handler(e)
     end
 
-    if PROFILE_REQUESTS
-      result = RubyProf.stop
-      printer = RubyProf::GraphHtmlPrinter.new(result)
-      printer.print(
-        File.open('/Projects/diesel/profile.html', 'w'),
-        min_percent: 5
-      )
-    end
-
-    Diesel.logger.info "#{out[0]} complete in #{Time.now - start_time}s"
-
+    stop_profiling_and_print if PROFILE_REQUESTS
+    log_request(request, out, (Time.now - start_time))
     out
+  end
+
+  def respond_with_http_basic_challenge
+    response = Rack::Response.new
+    response['WWW-Authenticate'] = %(Basic realm="Diesel HTTP Basic")
+    response.status = 401
+    response.headers['Content-Type'] = 'text/html'
+    response.write("Not Authorized\n")
+    out = response.finish
+  end
+
+  def respond_with_error_handler(e)
+    response = Rack::Response.new
+    response.headers['Content-Type'] = 'application/json'
+
+    begin
+      status, errors = JSON.parse(e.to_s) # TODO subclass StandardError
+      response.status = status;
+    rescue JSON::ParserError
+      response.status = 500
+      response.write({
+        status: 500,
+        error_class: e.class.to_s,
+        errors: e.to_s,
+        trace: e.backtrace
+      }.to_json)
+    else
+      response.write({
+        status: status,
+        error_class: 'Diesel Runtime Error',
+        errors: errors,
+        trace: e.backtrace
+      }.to_json)
+    end
+
+    out = response.finish
+  end
+
+  def start_profile
+    RubyProf.start
+  end
+
+  def stop_profiling_and_print
+    result = RubyProf.stop
+    printer = RubyProf::GraphHtmlPrinter.new(result)
+    printer.print(
+      File.open('/Projects/diesel/profile.html', 'w'),
+      min_percent: 5
+    )
+  end
+
+  def log_request(request, response, seconds)
+    Diesel.logger.info "#{request.request_method} #{request.path}"
+    Diesel.logger.info "PARAMS: #{request.params}"
+    Diesel.logger.info "#{response[0]} in #{seconds}s"
   end
 end
