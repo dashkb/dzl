9201c535ecd79b82abca89e6dfc5b4a3c2a7e138
Paul Bergeron <pbergeron@vitrue.com>
2012-03-06 15:02:57 -0500

    Split out list and condition doc functions into helpers for reuse.
    Added values, range support

--- lib/distil/doc.rb
+++ lib/distil/doc.rb
@@ -1,5 +1,5 @@
 module Distil::Doc
-
+  
   def to_md
     index_template = File.read("./template.erb")
 
@@ -7,23 +7,49 @@ module Distil::Doc
   end
 
   private
+  def doc_list(list)
+    # At this point, ranges were converted to arrays, so best guess
+    if list.all?{|e| e.class == Fixnum} && list.first < list.last
+      "#{list.first}..#{list.last}"
+    else
+      "#{list}"
+    end
+  end
+
+  def doc_conditions(conditions)
+    first_condition = conditions.shift
+    # All arrays have a size validiton, but the conditions are optional
+    doc_str = ""
+    if first_condition
+      doc_str = "Must be #{first_condition[0]} #{first_condition[1]}"
+      conditions.each do |condition|
+        doc_str += " and must be #{condition[0]} #{condition[1]}"
+      end
+      doc_str += "."
+    end
+    return doc_str
+  end
+
   def doc_param_type(type)
     "Format: #{type}"
   end
   
   def doc_param_size(size)
-    first_condition = size.conditions.shift
-    if first_condition
-      doc_str = "Size: Must be #{first_condition[0]} #{first_condition[1]}"
-      size.conditions.each do |condition|
-        doc_str += " and must be #{condition[0]} #{condition[1]}"
-      end
-      doc_str += "."
-    end
+    condition_doc = doc_conditions(size.conditions)
+    "Size: #{condition_doc}" unless condition_doc == ""
   end
 
   def doc_param_allowed_values(allowed_values)
-    "Allowed values: #{allowed_values}"
+    "Allowed values: #{doc_list(allowed_values)}"
+  end
+
+  def doc_param_disallowed_values(disallowed_values)
+    "Disallowed values: #{doc_list(disallowed_values)}"
+  end
+
+  def doc_param_value(value)
+    condition_doc = doc_conditions(value.conditions)
+    "Value: #{condition_doc}" unless condition_doc == ""
   end
 
   # Don't doc transforms
