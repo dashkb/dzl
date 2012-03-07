d5829541881ce53840add87dbb7030f76a1e160b
Kyle Brett <kyle@vitrue.com>
2012-03-07 11:40:23 -0500

    Spec for logging

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 require 'diesel/examples/base'
+require 'rack/test'
+
+class LogTestApp < Diesel::Examples::Base
+  get '/log_me' do
+    required :msg
+
+    handle do
+      logger.success.debug "message is #{params[:msg]}"
+    end
+  end
+end
 
 describe 'Modules including Diesel' do
   it 'should have a logger object provided by Diesel' do
@@ -20,5 +31,18 @@ describe 'Modules including Diesel' do
     end
 
     app.__logger.class.should == ActiveSupport::BufferedLogger
+  end
+end
+
+describe 'LogTestApp' do
+  include Rack::Test::Methods
+
+  def app; LogTestApp; end
+
+  it 'drops logs' do
+    get('/log_me?msg=success')
+    last_response.status.should == 200
+
+    `tail -n 1 #{LogTestApp.root}/log/success.test.log`.match('success').should_not == nil
   end
 end
