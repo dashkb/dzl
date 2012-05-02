3b050fba3a15954b19ffd654175d651f984559f4
Reed Lipman <rmlipman@gmail.com>
2012-05-02 15:35:24 -0400

    Symbolize keys in arrays of hashes

--- lib/dzl/core_ext.rb
+++ lib/dzl/core_ext.rb
@@ -6,10 +6,20 @@ class Hash
   def recursively_symbolize_keys!
     self.symbolize_keys!
     self.values.each do |v|
-      if v.is_a?(Hash)
+      if v.is_a?(Hash) || v.is_a?(Array)
         v.recursively_symbolize_keys!
       end
     end
     self
+  end
+end
+
+class Array
+  def recursively_symbolize_keys!
+    self.each do |item|
+      if item.is_a?(Hash) || item.is_a?(Array)
+        item.recursively_symbolize_keys!
+      end
+    end
   end
 end
