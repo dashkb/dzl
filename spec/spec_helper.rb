b89fd4157d04e5085ed8a8e5c45184d5d820c358
Kyle Brett <kyle@kylebrett.com>
2012-07-17 18:42:40 -0700

    A little hotness in the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
 require 'bundler/setup'
 require 'dzl'
+require 'pry'
 
 ENV['RACK_ENV'] ||= 'test'
 
+
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
   config.mock_with :rspec
+
+  config.order = 'random'
 end
